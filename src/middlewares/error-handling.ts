e694881c404500d3a2a1b355f8ec3940cacf9f60
riqueamais <henrique.quintino99@gmail.com>
2025-07-30 18:34:56 -0300

    feat: add error handling middleware and AppError class

---
+++ src/middlewares/error-handling.ts
@@ -0,0 +1,19 @@
+import e, { Request, Response, NextFunction } from "express";
+import { AppError } from "@/utils/AppError";
+
+export function errorHandling(
+  error: any,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
+
+  return response.status(500).json({
+    message: error.message,
+  });
+}
