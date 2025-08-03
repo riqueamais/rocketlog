8af598facbcef20b6f5ab07e1f9bf734aa9caa89
riqueamais <henrique.quintino99@gmail.com>
2025-08-03 15:46:19 -0300

    feat: add deliveries and deliveries status controllers with CRUD operations

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -1,15 +1,18 @@
 import { Router } from "express";
 
-import { DeliveriesController } from "../controllers/deliveries-controller";
+import { DeliveriesController } from "../controllers/Deliveries/deliveries-controller";
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
+import { DeliveriesStatusController } from "@/controllers/Deliveries/deliveries-status-controller";
 
 const deliveriesRoutes = Router();
 const deliveriesController = new DeliveriesController();
+ const deliveriesStatusController = new DeliveriesStatusController();
 
 deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]));
 
 deliveriesRoutes.post("/", deliveriesController.create);
 deliveriesRoutes.get("/", deliveriesController.index);
+deliveriesRoutes.patch("/:id/status", deliveriesStatusController.update);
 
 export { deliveriesRoutes };
