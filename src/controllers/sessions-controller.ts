2149bc9fa0af54ba187b5e5ed4c8a2ff0fe9a95d
riqueamais <henrique.quintino99@gmail.com>
2025-08-02 22:35:21 -0300

    feat: add JWT authentication with session creation logic and environment configuration

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,10 +1,42 @@
 import { Request, Response, NextFunction } from "express";
+import { prisma } from "@/database/prisma";
+import { sign } from "jsonwebtoken";
+import { z } from "zod";
+import { AppError } from "@/utils/AppError";
+import { compare } from "bcrypt";
+import { authConfig } from "@/config/auth";
 
 class SessionsController {
   async create(request: Request, response: Response, next: NextFunction) {
-    return response.status(501).json({
-      message: "Session creation not implemented yet",
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6),
     });
+
+    const { email, password } = bodySchema.parse(request.body);
+
+    const user = await prisma.user.findFirst({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new AppError("Invalid credentials", 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError("Invalid credentials", 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({ role: user.role ?? "customer" }, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return response.status(200).json({ token });
   }
 }
 
