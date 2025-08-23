6916a9c9e46c97ec366ef7673f88b4c979c2786f
riqueamais <henrique.quintino99@gmail.com>
2025-08-23 12:57:20 -0300

    feat: test user controller

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,5 +1,48 @@
+import request from "supertest";
+import { prisma } from "@/database/prisma";
+
+import { app } from "@/app";
+
 describe("UsersController", () => {
-  it("test", async () => {
-    console.log("Testing UsersController");
+  let user_id: string;
+
+  afterAll(async () => {
+    await prisma.user.delete({ where: { id: user_id } });
   });
+
+  it("should create a new user successfully", async () => {
+    const response = await request(app).post("/users").send({
+      name: "Test User",
+      email: "testuser@example.com",
+      password: "password123",
+    });
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.name).toBe("Test User");
+
+    user_id = response.body.id;
+  });
+
+  it("should throw new an error if user with same email already exists", async () => {
+    const response = await request(app).post("/users").send({
+      name: "Duplicate User",
+      email: "testuser@example.com",
+      password: "password123",
+    });
+
+    expect(response.status).toBe(400)
+    expect(response.body.message).toBe("Email already in use");
+  })
+
+  it("should throw a validation error if email is invalid", async () => {
+    const response = await request(app).post("/users").send({
+      name: "Test User",
+      email: "invalid-email",
+      password: "password123",
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe("validation error")
+  })
 });
